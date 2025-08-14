1daef10eba2e5efe3e78672e1a4288602f183ee9
Randy Brown <thatrandybrown@gmail.com>
2025-08-13 20:32:41 -0500

    add method to fetch photos

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,16 @@
+import * as MediaLibrary from 'expo-media-library';
 import { Text, View } from "react-native";
 
 export default function Index() {
+  const loadAllPhotos = async () => {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status === 'granted') {
+      const assets = await MediaLibrary.getAssetsAsync({ mediaType: 'photo', first: 100 });
+      console.log(`Found ${assets.totalCount} photos`);
+      console.log('First 100 photos:', assets.assets);
+    }
+  };
+
   return (
     <View
       style={{
